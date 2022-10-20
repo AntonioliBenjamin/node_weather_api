4d1c7f5961d44580c71d76e5d85d930b0017becf
benjamin antonioli <benantonioli@outlook.fr>
2022-10-20 17:35:11 +0200

    update_random_function

--- index.js
+++ index.js
@@ -5,31 +5,33 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
-
 app.get('/status', (req, res) => {
-    res.status(200).send('ok')
+    return res.status(200).send('ok')
 })
 
-app.get('/weather/:id?', async (req, res) => {
-    try {
-    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${req.params.id}&aqi=no`);
-    res.send({ 
+app.get('/weather/:city?', async (req, res) => {
+try {
+    const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${req.params.city}&aqi=no`);
+    return res.send({ 
             "temp_in_celsius": response.data.current.temp_c
-    });
-    } catch {
-        res.status(404).send({
+    })
+} catch {
+    return res.status(404).send({
             message: 'Ville introuvable'
         })
     }
 })
 
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * max -1)
+}
+
 app.get('/quoteday', async (req, res) => {
     const response = await axios.get('https://zenquotes.io/api/quotes')
-    res.send({
-        "quoteday": response.data.map(item  => item.q)[Math.floor(Math.random() * response.data.length)]
+    return res.send({
+        "quoteday": response.data.map(item  => item.q)[getRandomIndex(response.data.length)]
     })
 })
-
 
 app.listen(port, () => {
   console.log(`app listening on secret port`)
